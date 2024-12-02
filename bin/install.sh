11340f571e51bb0243d593e7eadb1a2575cd25f0
Rez Khaleghi | PocketJack <79227158+rezkhaleghi@users.noreply.github.com>
2024-12-02 14:40:32 +0300

    Update and rename installer.sh to install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Update package list and install Rust
-# echo "Installing Rust..."
-# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-# source $HOME/.cargo/env
+echo "Installing Rust..."
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source $HOME/.cargo/env
 
 # Install yt-dlp
 echo "Installing yt-dlp..."
